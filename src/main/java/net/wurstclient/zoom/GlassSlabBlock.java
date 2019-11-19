2ad0298b9c466b5aedb2b8e2bc1b280a1ddc3005
Alexander01998 <contact.wurstimperium@gmail.com>
2019-11-19 02:44:59 +0200

    Add glass slab

---
+++ src/main/java/net/wurstclient/zoom/GlassSlabBlock.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (C) 2019 | Wurst-Imperium | All rights reserved.
+ *
+ * This source code is subject to the terms of the GNU General Public
+ * License, version 3. If a copy of the GPL was not distributed with this
+ * file, You can obtain one at: https://www.gnu.org/licenses/gpl-3.0.txt
+ */
+package net.wurstclient.zoom;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.block.BlockRenderLayer;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.SlabBlock;
+import net.minecraft.entity.EntityType;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.BlockView;
+
+public final class GlassSlabBlock extends SlabBlock
+{
+	public GlassSlabBlock(Settings block$Settings_1)
+	{
+		super(block$Settings_1);
+	}
+	
+	@Override
+	public BlockRenderLayer getRenderLayer()
+	{
+		return BlockRenderLayer.TRANSLUCENT;
+	}
+	
+	@Override
+	@Environment(EnvType.CLIENT)
+	public boolean isSideInvisible(BlockState blockState_1,
+		BlockState blockState_2, Direction direction_1)
+	{
+		return blockState_2.getBlock() == this && blockState_1
+			.get(SlabBlock.TYPE) == blockState_2.get(SlabBlock.TYPE) ? true
+				: super.isSideInvisible(blockState_1, blockState_2,
+					direction_1);
+	}
+	
+	@Override
+	@Environment(EnvType.CLIENT)
+	public float getAmbientOcclusionLightLevel(BlockState blockState_1,
+		BlockView blockView_1, BlockPos blockPos_1)
+	{
+		return 1.0F;
+	}
+	
+	@Override
+	public boolean isTranslucent(BlockState blockState_1, BlockView blockView_1,
+		BlockPos blockPos_1)
+	{
+		return true;
+	}
+	
+	@Override
+	public boolean canSuffocate(BlockState blockState_1, BlockView blockView_1,
+		BlockPos blockPos_1)
+	{
+		return false;
+	}
+	
+	@Override
+	public boolean isSimpleFullBlock(BlockState blockState_1,
+		BlockView blockView_1, BlockPos blockPos_1)
+	{
+		return false;
+	}
+	
+	@Override
+	public boolean allowsSpawning(BlockState blockState_1,
+		BlockView blockView_1, BlockPos blockPos_1, EntityType<?> entityType_1)
+	{
+		return false;
+	}
+}
--- src/main/java/net/wurstclient/zoom/MoGlass.java
+++ src/main/java/net/wurstclient/zoom/MoGlass.java
@@ -7,14 +7,33 @@
  */
 package net.wurstclient.zoom;
 
+import net.fabricmc.fabric.api.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
 public enum MoGlass
 {
 	INSTANCE;
 	
 	public static final String VERSION = "1.0";
 	
+	public static final Block GLASS_SLAB =
+		new GlassSlabBlock(FabricBlockSettings.of(Material.GLASS).hardness(0.3F)
+			.resistance(0.3F).sounds(BlockSoundGroup.GLASS).build());
+	
 	public void initialize()
 	{
 		System.out.println("Starting Mo Glass...");
+		
+		Identifier glassSlabId = new Identifier("mo_glass", "glass_slab");
+		Registry.register(Registry.BLOCK, glassSlabId, GLASS_SLAB);
+		Registry.register(Registry.ITEM, glassSlabId, new BlockItem(GLASS_SLAB,
+			new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
 	}
 }
