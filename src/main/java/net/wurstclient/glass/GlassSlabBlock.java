729e4cf17d2133bdfb8245c7a63ed91b6abe9f34
Alexander01998 <contact.wurstimperium@gmail.com>
2019-12-01 15:03:55 +0200

    Improve transparency calculation in GlassSlabBlock

--- src/main/java/net/wurstclient/glass/GlassSlabBlock.java
+++ src/main/java/net/wurstclient/glass/GlassSlabBlock.java
@@ -11,7 +11,9 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.block.BlockRenderLayer;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.SlabBlock;
+import net.minecraft.block.enums.SlabType;
 import net.minecraft.entity.EntityType;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
@@ -35,11 +37,56 @@ public final class GlassSlabBlock extends SlabBlock
 	public boolean isSideInvisible(BlockState blockState_1,
 		BlockState blockState_2, Direction direction_1)
 	{
-		if(blockState_2.getBlock() == this && blockState_1
-			.get(SlabBlock.TYPE) == blockState_2.get(SlabBlock.TYPE))
+		if(blockState_2.getBlock() == Blocks.GLASS)
 			return true;
 		
+		if(blockState_2.getBlock() == this)
+			if(isInvisibleToGlassSlab(blockState_1, blockState_2, direction_1))
+				return true;
+			
+		if(blockState_2.getBlock() == MoGlass.GLASS_STAIRS)
+			if(isInvisibleToGlassStairs(blockState_1, blockState_2,
+				direction_1))
+				return true;
+			
 		return super.isSideInvisible(blockState_1, blockState_2, direction_1);
+	}
+	
+	private boolean isInvisibleToGlassSlab(BlockState blockState_1,
+		BlockState blockState_2, Direction direction_1)
+	{
+		SlabType type1 = blockState_1.get(SlabBlock.TYPE);
+		SlabType type2 = blockState_2.get(SlabBlock.TYPE);
+		
+		if(type2 == SlabType.DOUBLE)
+			return true;
+		
+		switch(direction_1)
+		{
+			case UP:
+			case DOWN:
+			if(type1 != type2)
+				return true;
+			break;
+			
+			case NORTH:
+			case EAST:
+			case SOUTH:
+			case WEST:
+			if(type1 == type2)
+				return true;
+			break;
+		}
+		
+		return false;
+	}
+	
+	private boolean isInvisibleToGlassStairs(BlockState blockState_1,
+		BlockState blockState_2, Direction direction_1)
+	{
+		// TODO
+		
+		return false;
 	}
 	
 	@Override
