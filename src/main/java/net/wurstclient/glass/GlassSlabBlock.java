4f606e3fdb9622c502631167944ba6c783a01321
Alexander01998 <contact.wurstimperium@gmail.com>
2019-12-01 16:51:47 +0200

    Implement GlassSlabBlock.isinvisibleToStairs()

--- src/main/java/net/wurstclient/glass/GlassSlabBlock.java
+++ src/main/java/net/wurstclient/glass/GlassSlabBlock.java
@@ -13,6 +13,8 @@ import net.minecraft.block.BlockRenderLayer;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.SlabBlock;
+import net.minecraft.block.StairsBlock;
+import net.minecraft.block.enums.BlockHalf;
 import net.minecraft.block.enums.SlabType;
 import net.minecraft.entity.EntityType;
 import net.minecraft.util.math.BlockPos;
@@ -84,8 +86,31 @@ public final class GlassSlabBlock extends SlabBlock
 	private boolean isInvisibleToGlassStairs(BlockState blockState_1,
 		BlockState blockState_2, Direction direction_1)
 	{
-		// TODO
+		SlabType type1 = blockState_1.get(SlabBlock.TYPE);
+		BlockHalf half2 = blockState_2.get(StairsBlock.HALF);
+		Direction facing2 = blockState_2.get(StairsBlock.FACING);
 		
+		// up
+		if(direction_1 == Direction.UP)
+			if(half2 == BlockHalf.BOTTOM)
+				return true;
+			
+		// down
+		if(direction_1 == Direction.DOWN)
+			if(half2 == BlockHalf.TOP)
+				return true;
+			
+		// other stairs rear
+		if(facing2 == direction_1.getOpposite())
+			return true;
+		
+		// sides
+		if(direction_1.getHorizontal() != -1)
+			if(type1 == SlabType.BOTTOM && half2 == BlockHalf.BOTTOM)
+				return true;
+			else if(type1 == SlabType.TOP && half2 == BlockHalf.TOP)
+				return true;
+			
 		return false;
 	}
 	
