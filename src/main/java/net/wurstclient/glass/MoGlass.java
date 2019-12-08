9559d87d8ac4e005233d3e8b4205308c85a39a1d
Alexander01998 <contact.wurstimperium@gmail.com>
2019-12-08 21:43:37 +0100

    Change version to 1.1

--- src/main/java/net/wurstclient/glass/MoGlass.java
+++ src/main/java/net/wurstclient/glass/MoGlass.java
@@ -22,7 +22,7 @@ public enum MoGlass
 {
 	INSTANCE;
 	
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.1";
 	
 	public static final Block GLASS_SLAB =
 		new GlassSlabBlock(FabricBlockSettings.of(Material.GLASS).hardness(0.3F)
