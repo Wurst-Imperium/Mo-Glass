2399ed982c9cb4cd0ce7915a5cdc89e8c4453ff3
Alexander01998 <contact.wurstimperium@gmail.com>
2020-07-05 20:09:46 +0200

    Add stained glass slabs

--- src/main/java/net/wurstclient/glass/MoGlass.java
+++ src/main/java/net/wurstclient/glass/MoGlass.java
@@ -19,6 +19,7 @@ import net.minecraft.item.Item;
 import net.minecraft.item.Item.Settings;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.DyeColor;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.registry.Registry;
@@ -38,6 +39,49 @@ public enum MoGlass
 			.allowsSpawning(MoGlass::never).solidBlock(MoGlass::never)
 			.suffocates(MoGlass::never));
 	
+	public static final StainedGlassSlabBlock WHITE_STAINED_GLASS_SLAB =
+		createStainedGlassSlab(DyeColor.WHITE);
+	public static final StainedGlassSlabBlock ORANGE_STAINED_GLASS_SLAB =
+		createStainedGlassSlab(DyeColor.ORANGE);
+	public static final StainedGlassSlabBlock MAGENTA_STAINED_GLASS_SLAB =
+		createStainedGlassSlab(DyeColor.MAGENTA);
+	public static final StainedGlassSlabBlock LIGHT_BLUE_STAINED_GLASS_SLAB =
+		createStainedGlassSlab(DyeColor.LIGHT_BLUE);
+	public static final StainedGlassSlabBlock YELLOW_STAINED_GLASS_SLAB =
+		createStainedGlassSlab(DyeColor.YELLOW);
+	public static final StainedGlassSlabBlock LIME_STAINED_GLASS_SLAB =
+		createStainedGlassSlab(DyeColor.LIME);
+	public static final StainedGlassSlabBlock PINK_STAINED_GLASS_SLAB =
+		createStainedGlassSlab(DyeColor.PINK);
+	public static final StainedGlassSlabBlock GRAY_STAINED_GLASS_SLAB =
+		createStainedGlassSlab(DyeColor.GRAY);
+	public static final StainedGlassSlabBlock LIGHT_GRAY_STAINED_GLASS_SLAB =
+		createStainedGlassSlab(DyeColor.LIGHT_GRAY);
+	public static final StainedGlassSlabBlock CYAN_STAINED_GLASS_SLAB =
+		createStainedGlassSlab(DyeColor.CYAN);
+	public static final StainedGlassSlabBlock PURPLE_STAINED_GLASS_SLAB =
+		createStainedGlassSlab(DyeColor.PURPLE);
+	public static final StainedGlassSlabBlock BLUE_STAINED_GLASS_SLAB =
+		createStainedGlassSlab(DyeColor.BLUE);
+	public static final StainedGlassSlabBlock BROWN_STAINED_GLASS_SLAB =
+		createStainedGlassSlab(DyeColor.BROWN);
+	public static final StainedGlassSlabBlock GREEN_STAINED_GLASS_SLAB =
+		createStainedGlassSlab(DyeColor.GREEN);
+	public static final StainedGlassSlabBlock RED_STAINED_GLASS_SLAB =
+		createStainedGlassSlab(DyeColor.RED);
+	public static final StainedGlassSlabBlock BLACK_STAINED_GLASS_SLAB =
+		createStainedGlassSlab(DyeColor.BLACK);
+	
+	public static final StainedGlassSlabBlock[] STAINED_GLASS_SLABS =
+		{WHITE_STAINED_GLASS_SLAB, ORANGE_STAINED_GLASS_SLAB,
+			MAGENTA_STAINED_GLASS_SLAB, LIGHT_BLUE_STAINED_GLASS_SLAB,
+			YELLOW_STAINED_GLASS_SLAB, LIME_STAINED_GLASS_SLAB,
+			PINK_STAINED_GLASS_SLAB, GRAY_STAINED_GLASS_SLAB,
+			LIGHT_GRAY_STAINED_GLASS_SLAB, CYAN_STAINED_GLASS_SLAB,
+			PURPLE_STAINED_GLASS_SLAB, BLUE_STAINED_GLASS_SLAB,
+			BROWN_STAINED_GLASS_SLAB, GREEN_STAINED_GLASS_SLAB,
+			RED_STAINED_GLASS_SLAB, BLACK_STAINED_GLASS_SLAB};
+	
 	public static final Block GLASS_STAIRS =
 		new GlassStairsBlock(AbstractBlock.Settings.of(Material.GLASS)
 			.strength(0.3F).sounds(BlockSoundGroup.GLASS).nonOpaque()
@@ -49,7 +93,16 @@ public enum MoGlass
 		this.client = client;
 		System.out.println("Starting Mo Glass...");
 		
+		String[] colors = {"white", "orange", "magenta", "light_blue", "yellow",
+			"lime", "pink", "gray", "light_gray", "cyan", "purple", "blue",
+			"brown", "green", "red", "black"};
+		
 		registerBlock(GLASS_SLAB, "glass_slab", ItemGroup.BUILDING_BLOCKS);
+		
+		for(int i = 0; i < 16; i++)
+			registerBlock(STAINED_GLASS_SLABS[i],
+				colors[i] + "_stained_glass_slab", ItemGroup.BUILDING_BLOCKS);
+		
 		registerBlock(GLASS_STAIRS, "glass_stairs", ItemGroup.BUILDING_BLOCKS);
 	}
 	
@@ -65,6 +118,15 @@ public enum MoGlass
 		if(client)
 			BlockRenderLayerMap.INSTANCE.putBlock(block,
 				RenderLayer.getTranslucent());
+	}
+	
+	private static StainedGlassSlabBlock createStainedGlassSlab(DyeColor color)
+	{
+		return new StainedGlassSlabBlock(color,
+			AbstractBlock.Settings.of(Material.GLASS, color).strength(0.3F)
+				.sounds(BlockSoundGroup.GLASS).nonOpaque()
+				.allowsSpawning(MoGlass::never).solidBlock(MoGlass::never)
+				.suffocates(MoGlass::never).blockVision(MoGlass::never));
 	}
 	
 	// Copies of the Blocks.never() methods because the originals are not
