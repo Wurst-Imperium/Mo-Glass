5273cde4d47b3c53dc823c5c4cc6a70a440ad297
Alexander01998 <contact.wurstimperium@gmail.com>
2021-12-16 02:53:03 +0100

    Remove "unnecessary" @SuppressWarnings("deprecation")
    Apparently this is necessary in 1.18, but not in 1.17.1, even though
    it's deprecated in both versions. ¯\_(ツ)_/¯

--- src/main/java/net/wurstclient/glass/GlassSlabBlock.java
+++ src/main/java/net/wurstclient/glass/GlassSlabBlock.java
@@ -9,7 +9,6 @@ package net.wurstclient.glass;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.SlabBlock;
@@ -27,13 +26,6 @@ public final class GlassSlabBlock extends SlabBlock
 		super(block$Settings_1);
 	}
 	
-	/**
-	 * isSideInvisible() seems to be deprecated for no good reason. It is still
-	 * used in
-	 * {@link Block#shouldDrawSide(BlockState, BlockView, BlockPos, Direction, BlockPos)}
-	 * and there is no replacement.
-	 */
-	@SuppressWarnings("deprecation")
 	@Override
 	@Environment(EnvType.CLIENT)
 	public boolean isSideInvisible(BlockState blockState_1,
--- src/main/java/net/wurstclient/glass/GlassStairsBlock.java
+++ src/main/java/net/wurstclient/glass/GlassStairsBlock.java
@@ -9,7 +9,6 @@ package net.wurstclient.glass;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.SlabBlock;
@@ -28,13 +27,6 @@ public final class GlassStairsBlock extends StairsBlock
 		super(Blocks.GLASS.getDefaultState(), block$Settings_1);
 	}
 	
-	/**
-	 * isSideInvisible() seems to be deprecated for no good reason. It is still
-	 * used in
-	 * {@link Block#shouldDrawSide(BlockState, BlockView, BlockPos, Direction, BlockPos)}
-	 * and there is no replacement.
-	 */
-	@SuppressWarnings("deprecation")
 	@Override
 	@Environment(EnvType.CLIENT)
 	public boolean isSideInvisible(BlockState blockState_1,
