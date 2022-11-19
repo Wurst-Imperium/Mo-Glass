394df0855a6ef2c94929c0c76570f5447567d974
Jakob Dickert <jakob.dickert@outlook.com>
2022-11-19 16:43:15 +0100

    Fix for Stained and tinted Slabs

--- src/main/java/net/wurstclient/glass/StainedGlassSlabBlock.java
+++ src/main/java/net/wurstclient/glass/StainedGlassSlabBlock.java
@@ -25,16 +25,14 @@ import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
 
-public final class StainedGlassSlabBlock extends SlabBlock implements Stainable
-{
+public final class StainedGlassSlabBlock extends SlabBlock implements Stainable {
 	private final DyeColor color;
-	
-	public StainedGlassSlabBlock(DyeColor color, Settings block$Settings_1)
-	{
+
+	public StainedGlassSlabBlock(DyeColor color, Settings block$Settings_1) {
 		super(block$Settings_1);
 		this.color = color;
 	}
-	
+
 	/**
 	 * isSideInvisible() seems to be deprecated for no good reason. It is still
 	 * used in
@@ -45,121 +43,111 @@ public final class StainedGlassSlabBlock extends SlabBlock implements Stainable
 	@Override
 	@Environment(EnvType.CLIENT)
 	public boolean isSideInvisible(BlockState blockState_1,
-		BlockState blockState_2, Direction direction_1)
-	{
+			BlockState blockState_2, Direction direction_1) {
 		Block block_2 = blockState_2.getBlock();
-		
-		if(block_2 instanceof StainedGlassBlock
-			&& ((StainedGlassBlock)block_2).getColor() == color)
+
+		if (block_2 instanceof StainedGlassBlock
+				&& ((StainedGlassBlock) block_2).getColor() == color)
 			return true;
-		
-		if(block_2 == this)
-			if(isInvisibleToGlassSlab(blockState_1, blockState_2, direction_1))
+
+		if (block_2 == this)
+			if (isInvisibleToGlassSlab(blockState_1, blockState_2, direction_1))
 				return true;
-			
-		if(block_2 instanceof StainedGlassStairsBlock
-			&& ((StainedGlassStairsBlock)block_2).getColor() == color)
-			if(isInvisibleToGlassStairs(blockState_1, blockState_2,
-				direction_1))
+
+		if (block_2 instanceof StainedGlassStairsBlock
+				&& ((StainedGlassStairsBlock) block_2).getColor() == color)
+			if (isInvisibleToGlassStairs(blockState_1, blockState_2,
+					direction_1))
 				return true;
-			
+
 		return super.isSideInvisible(blockState_1, blockState_2, direction_1);
 	}
-	
+
 	private boolean isInvisibleToGlassSlab(BlockState blockState_1,
-		BlockState blockState_2, Direction direction_1)
-	{
+			BlockState blockState_2, Direction direction_1) {
 		SlabType type1 = blockState_1.get(SlabBlock.TYPE);
 		SlabType type2 = blockState_2.get(SlabBlock.TYPE);
-		
-		if(type2 == SlabType.DOUBLE)
+
+		if (type2 == SlabType.DOUBLE)
 			return true;
-		
-		switch(direction_1)
-		{
+
+		switch (direction_1) {
 			case UP:
 			case DOWN:
-			if(type1 != type2)
-				return true;
-			break;
-			
+				if (type1 != type2)
+					return true && !blockState_2.hasSidedTransparency();
+				break;
+
 			case NORTH:
 			case EAST:
 			case SOUTH:
 			case WEST:
-			if(type1 == type2)
-				return true;
-			break;
+				if (type1 == type2)
+					return true && !blockState_2.hasSidedTransparency();
+				break;
 		}
-		
+
 		return false;
 	}
-	
+
 	private boolean isInvisibleToGlassStairs(BlockState blockState_1,
-		BlockState blockState_2, Direction direction_1)
-	{
+			BlockState blockState_2, Direction direction_1) {
 		SlabType type1 = blockState_1.get(SlabBlock.TYPE);
 		BlockHalf half2 = blockState_2.get(StairsBlock.HALF);
 		Direction facing2 = blockState_2.get(StairsBlock.FACING);
-		
+
 		// up
-		if(direction_1 == Direction.UP)
-			if(half2 == BlockHalf.BOTTOM)
+		if (direction_1 == Direction.UP)
+			if (half2 == BlockHalf.BOTTOM)
 				return true;
-			
+
 		// down
-		if(direction_1 == Direction.DOWN)
-			if(half2 == BlockHalf.TOP)
+		if (direction_1 == Direction.DOWN)
+			if (half2 == BlockHalf.TOP)
 				return true;
-			
+
 		// other stairs rear
-		if(facing2 == direction_1.getOpposite())
+		if (facing2 == direction_1.getOpposite())
 			return true;
-		
+
 		// sides
-		if(direction_1.getHorizontal() != -1)
-		{
-			if(type1 == SlabType.BOTTOM && half2 == BlockHalf.BOTTOM)
+		if (direction_1.getHorizontal() != -1) {
+			if (type1 == SlabType.BOTTOM && half2 == BlockHalf.BOTTOM)
 				return true;
-			
-			if(type1 == SlabType.TOP && half2 == BlockHalf.TOP)
+
+			if (type1 == SlabType.TOP && half2 == BlockHalf.TOP)
 				return true;
 		}
-		
+
 		return false;
 	}
-	
+
 	@Override
 	public VoxelShape getCameraCollisionShape(BlockState state, BlockView world,
-		BlockPos pos, ShapeContext context)
-	{
+			BlockPos pos, ShapeContext context) {
 		return VoxelShapes.empty();
 	}
-	
+
 	@Override
 	@Environment(EnvType.CLIENT)
 	public float getAmbientOcclusionLightLevel(BlockState blockState_1,
-		BlockView blockView_1, BlockPos blockPos_1)
-	{
+			BlockView blockView_1, BlockPos blockPos_1) {
 		return 1.0F;
 	}
-	
+
 	@Override
 	public boolean isTranslucent(BlockState blockState_1, BlockView blockView_1,
-		BlockPos blockPos_1)
-	{
+			BlockPos blockPos_1) {
 		return true;
 	}
-	
+
 	public boolean isSimpleFullBlock(BlockState blockState_1,
-		BlockView blockView_1, BlockPos blockPos_1)
-	{
+			BlockView blockView_1, BlockPos blockPos_1) {
 		return false;
 	}
-	
+
 	@Override
-	public DyeColor getColor()
-	{
+	public DyeColor getColor() {
 		return color;
 	}
 }
--- src/main/java/net/wurstclient/glass/TintedGlassSlabBlock.java
+++ src/main/java/net/wurstclient/glass/TintedGlassSlabBlock.java
@@ -23,13 +23,11 @@ import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
 
-public final class TintedGlassSlabBlock extends SlabBlock
-{
-	public TintedGlassSlabBlock(Settings block$Settings_1)
-	{
+public final class TintedGlassSlabBlock extends SlabBlock {
+	public TintedGlassSlabBlock(Settings block$Settings_1) {
 		super(block$Settings_1);
 	}
-	
+
 	/**
 	 * isSideInvisible() seems to be deprecated for no good reason. It is still
 	 * used in
@@ -40,118 +38,108 @@ public final class TintedGlassSlabBlock extends SlabBlock
 	@Override
 	@Environment(EnvType.CLIENT)
 	public boolean isSideInvisible(BlockState blockState_1,
-		BlockState blockState_2, Direction direction_1)
-	{
-		if(blockState_2.getBlock() == Blocks.TINTED_GLASS)
+			BlockState blockState_2, Direction direction_1) {
+		if (blockState_2.getBlock() == Blocks.TINTED_GLASS)
 			return true;
-		
-		if(blockState_2.getBlock() == this)
-			if(isInvisibleToGlassSlab(blockState_1, blockState_2, direction_1))
+
+		if (blockState_2.getBlock() == this)
+			if (isInvisibleToGlassSlab(blockState_1, blockState_2, direction_1))
 				return true;
-			
-		if(blockState_2.getBlock() == MoGlassBlocks.TINTED_GLASS_STAIRS)
-			if(isInvisibleToGlassStairs(blockState_1, blockState_2,
-				direction_1))
+
+		if (blockState_2.getBlock() == MoGlassBlocks.TINTED_GLASS_STAIRS)
+			if (isInvisibleToGlassStairs(blockState_1, blockState_2,
+					direction_1))
 				return true;
-			
+
 		return super.isSideInvisible(blockState_1, blockState_2, direction_1);
 	}
-	
+
 	private boolean isInvisibleToGlassSlab(BlockState blockState_1,
-		BlockState blockState_2, Direction direction_1)
-	{
+			BlockState blockState_2, Direction direction_1) {
 		SlabType type1 = blockState_1.get(SlabBlock.TYPE);
 		SlabType type2 = blockState_2.get(SlabBlock.TYPE);
-		
-		if(type2 == SlabType.DOUBLE)
+
+		if (type2 == SlabType.DOUBLE)
 			return true;
-		
-		switch(direction_1)
-		{
+
+		switch (direction_1) {
 			case UP:
 			case DOWN:
-			if(type1 != type2)
-				return true;
-			break;
-			
+				if (type1 != type2)
+					return true && !blockState_2.hasSidedTransparency();
+				break;
+
 			case NORTH:
 			case EAST:
 			case SOUTH:
 			case WEST:
-			if(type1 == type2)
-				return true;
-			break;
+				if (type1 == type2)
+					return true && !blockState_2.hasSidedTransparency();
+				break;
 		}
-		
+
 		return false;
 	}
-	
+
 	private boolean isInvisibleToGlassStairs(BlockState blockState_1,
-		BlockState blockState_2, Direction direction_1)
-	{
+			BlockState blockState_2, Direction direction_1) {
 		SlabType type1 = blockState_1.get(SlabBlock.TYPE);
 		BlockHalf half2 = blockState_2.get(StairsBlock.HALF);
 		Direction facing2 = blockState_2.get(StairsBlock.FACING);
-		
+
 		// up
-		if(direction_1 == Direction.UP)
-			if(half2 == BlockHalf.BOTTOM)
+		if (direction_1 == Direction.UP)
+			if (half2 == BlockHalf.BOTTOM)
 				return true;
-			
+
 		// down
-		if(direction_1 == Direction.DOWN)
-			if(half2 == BlockHalf.TOP)
+		if (direction_1 == Direction.DOWN)
+			if (half2 == BlockHalf.TOP)
 				return true;
-			
+
 		// other stairs rear
-		if(facing2 == direction_1.getOpposite())
+		if (facing2 == direction_1.getOpposite())
 			return true;
-		
+
 		// sides
-		if(direction_1.getHorizontal() != -1)
-		{
-			if(type1 == SlabType.BOTTOM && half2 == BlockHalf.BOTTOM)
+		if (direction_1.getHorizontal() != -1) {
+			if (type1 == SlabType.BOTTOM && half2 == BlockHalf.BOTTOM)
 				return true;
-			
-			if(type1 == SlabType.TOP && half2 == BlockHalf.TOP)
+
+			if (type1 == SlabType.TOP && half2 == BlockHalf.TOP)
 				return true;
 		}
-		
+
 		return false;
 	}
-	
+
 	@Override
 	public VoxelShape getCameraCollisionShape(BlockState state, BlockView world,
-		BlockPos pos, ShapeContext context)
-	{
+			BlockPos pos, ShapeContext context) {
 		return VoxelShapes.empty();
 	}
-	
+
 	@Override
 	@Environment(EnvType.CLIENT)
 	public float getAmbientOcclusionLightLevel(BlockState blockState_1,
-		BlockView blockView_1, BlockPos blockPos_1)
-	{
+			BlockView blockView_1, BlockPos blockPos_1) {
 		return 1.0F;
 	}
-	
+
 	public boolean isSimpleFullBlock(BlockState blockState_1,
-		BlockView blockView_1, BlockPos blockPos_1)
-	{
+			BlockView blockView_1, BlockPos blockPos_1) {
 		return false;
 	}
-	
+
 	@Override
 	public boolean isTranslucent(BlockState blockState_1, BlockView blockView_1,
-		BlockPos blockPos_1)
-	{
+			BlockPos blockPos_1) {
 		return false;
 	}
-	
+
 	@Override
-	public int getOpacity(BlockState state, BlockView world, BlockPos pos)
-	{
+	public int getOpacity(BlockState state, BlockView world, BlockPos pos) {
 		return state.get(TYPE) == SlabType.DOUBLE ? world.getMaxLightLevel()
-			: 0;
+				: 0;
 	}
 }
