348eeae1386b058fe71e2b51fbc3ed4eba5f5065
Alexander01998 <contact.wurstimperium@gmail.com>
2023-12-13 11:44:15 +0100

    Fix crash when direction is up or down

--- src/main/java/net/wurstclient/glass/GlassStairsBlock.java
+++ src/main/java/net/wurstclient/glass/GlassStairsBlock.java
@@ -234,10 +234,10 @@ public final class GlassStairsBlock extends StairsBlock
 			return true;
 		
 		// other curved stairs fully covered side
-		if(facingFrom == direction.rotateYClockwise()
+		if(facingFrom.rotateYCounterclockwise() == direction
 			&& shapeFrom == StairShape.INNER_RIGHT)
 			return true;
-		if(facingFrom == direction.rotateYCounterclockwise()
+		if(facingFrom.rotateYClockwise() == direction
 			&& shapeFrom == StairShape.INNER_LEFT)
 			return true;
 		
