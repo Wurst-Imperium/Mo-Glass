7584de7cc285c04c167396c1f9c9b451bd53d34b
Alexander01998 <contact.wurstimperium@gmail.com>
2025-03-05 19:30:31 +0100

    Update generate_recipes.py script to match current recipe format

--- scripts/generate_recipes.py
+++ scripts/generate_recipes.py
@@ -1,9 +1,6 @@
-# To generate recipes, place this script in the data/mo_glass/recipes folder and run it.
-
-import os
 import json
 
-cwd = os.path.dirname(os.path.realpath(__file__))
+recipes = "src/main/resources/data/mo_glass/recipe"
 minecraft_colors = ["white", "orange", "magenta", "light_blue", "yellow", "lime", "pink", "gray", "light_gray", "cyan", "purple", "blue", "brown", "green", "red", "black"]
 
 slab_crafting_template = {
@@ -11,9 +8,7 @@ slab_crafting_template = {
 	"category": "building",
 	"group": "stained_glass_slab",
 	"key": {
-		"#": {
-			"item": "minecraft:glass"
-		}
+		"#": "minecraft:glass"
 	},
 	"pattern": [
 		"###"
@@ -26,9 +21,7 @@ slab_crafting_template = {
 
 slab_from_glass_stonecutting_template = {
 	"type": "minecraft:stonecutting",
-	"ingredient": {
-		"item": "minecraft:glass"
-	},
+	"ingredient": "minecraft:glass",
 	"result": {
 		"count": 2,
 		"id": "mo_glass:glass_slab"
@@ -40,9 +33,7 @@ stairs_crafting_template = {
 	"category": "building",
 	"group": "stained_glass_stairs",
 	"key": {
-		"#": {
-			"item": "minecraft:glass"
-		}
+		"#": "minecraft:glass"
 	},
 	"pattern": [
 		"#  ",
@@ -57,9 +48,7 @@ stairs_crafting_template = {
 
 stairs_from_glass_stonecutting_template = {
 	"type": "minecraft:stonecutting",
-	"ingredient": {
-		"item": "minecraft:glass"
-	},
+	"ingredient": "minecraft:glass",
 	"result": {
 		"count": 1,
 		"id": "mo_glass:glass_stairs"
@@ -67,41 +56,40 @@ stairs_from_glass_stonecutting_template = {
 }
 
 def create_recipes(block, stairs, slab):
-	slab_file = f"{cwd}/{slab}.json"
+	slab_file = f"{recipes}/{slab}.json"
 	with open(slab_file, "w") as f:
 		recipe = dict(slab_crafting_template)
-		recipe["key"]["#"]["item"] = f"minecraft:{block}"
+		recipe["key"]["#"] = f"minecraft:{block}"
 		recipe["result"]["id"] = f"mo_glass:{slab}"
-		if not "stained" in block:
+		if "stained" not in block:
 			del recipe["group"]
-		json.dump(recipe, f, indent=2)
+		f.write(json.dumps(recipe, indent=2) + "\n")
 		print(f"Created {slab_file}")
-	slab_from_glass_stonecutting_file = f"{cwd}/{slab}_from_glass_stonecutting.json"
+	slab_from_glass_stonecutting_file = f"{recipes}/{slab}_from_glass_stonecutting.json"
 	with open(slab_from_glass_stonecutting_file, "w") as f:
 		recipe = dict(slab_from_glass_stonecutting_template)
-		recipe["ingredient"]["item"] = f"minecraft:{block}"
+		recipe["ingredient"] = f"minecraft:{block}"
 		recipe["result"]["id"] = f"mo_glass:{slab}"
-		json.dump(recipe, f, indent=2)
+		f.write(json.dumps(recipe, indent=2) + "\n")
 		print(f"Created {slab_from_glass_stonecutting_file}")
-	stairs_file = f"{cwd}/{stairs}.json"
+	stairs_file = f"{recipes}/{stairs}.json"
 	with open(stairs_file, "w") as f:
 		recipe = dict(stairs_crafting_template)
-		recipe["key"]["#"]["item"] = f"minecraft:{block}"
+		recipe["key"]["#"] = f"minecraft:{block}"
 		recipe["result"]["id"] = f"mo_glass:{stairs}"
-		if not "stained" in block:
+		if "stained" not in block:
 			del recipe["group"]
-		json.dump(recipe, f, indent=2)
+		f.write(json.dumps(recipe, indent=2) + "\n")
 		print(f"Created {stairs_file}")
-	stairs_from_glass_stonecutting_file = f"{cwd}/{stairs}_from_glass_stonecutting.json"
+	stairs_from_glass_stonecutting_file = f"{recipes}/{stairs}_from_glass_stonecutting.json"
 	with open(stairs_from_glass_stonecutting_file, "w") as f:
 		recipe = dict(stairs_from_glass_stonecutting_template)
-		recipe["ingredient"]["item"] = f"minecraft:{block}"
+		recipe["ingredient"] = f"minecraft:{block}"
 		recipe["result"]["id"] = f"mo_glass:{stairs}"
-		json.dump(recipe, f, indent=2)
+		f.write(json.dumps(recipe, indent=2) + "\n")
 		print(f"Created {stairs_from_glass_stonecutting_file}")
 
 create_recipes("glass", "glass_stairs", "glass_slab")
 create_recipes("tinted_glass", "tinted_glass_stairs", "tinted_glass_slab")
 for color in minecraft_colors:
 	create_recipes(f"{color}_stained_glass", f"{color}_stained_glass_stairs", f"{color}_stained_glass_slab")
-input("Done! Press enter to exit.")
